60630ed3569ebe7334c40f61e0524d9d6de2fcb6
Yuto Nakamura <yutonakamura0320@gmail.com>
2021-01-30 11:19:12 +0900

    feat(app.js): add a image, text, title

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,11 +18,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   leftContainer: {
-    backgroundColor: 'red',
     width: 100,
   },
   rightContainer: {
-    backgroundColor: 'blue',
     // 描画可能なエリアを可能な限り使用する
     flex: 1,
   },
@@ -32,8 +30,22 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}></View>
-        <View style={styles.rightContainer}></View>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{
+              uri: 'https://reactnative.dev/img/tiny_logo.png',
+            }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3}>
+            React Native combines the best parts of native development with
+            React, a best-in-class JavaScript library for building user
+            interfaces.
+          </Text>
+          <Text>ReactNews</Text>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
